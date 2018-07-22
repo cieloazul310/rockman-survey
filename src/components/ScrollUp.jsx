c1af38b49001011cf2d1e6d576e901995a7905b4
cieloazul310 <cieloazul310@gmail.com>
2018-07-22 14:15:51 +0900

    update

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import ScrollToTop from 'react-scroll-up';
 import { Glyphicon } from 'react-bootstrap';
 
+import './ScrollUp.css';
+
 const ScrollUp = (
   <ScrollToTop showUnder={160}>
-    <div
-      style={{
-        padding: '.5em',
-        color: 'white',
-        backgroundColor: 'rgba(80, 80, 80, 0.8)',
-        fontSize: '1.6em',
-        borderRadius: '25%'
-      }}
-    >
+    <div className="scroll-up-button" title="一番上にスクロール">
       <Glyphicon glyph="chevron-up" />
     </div>
   </ScrollToTop>
